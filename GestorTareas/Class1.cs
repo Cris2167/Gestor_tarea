e07ccd6e1bbcb3d1571819e554199c7c59c67f59
Cris2167 <165099400+Cris2167@users.noreply.github.com>
2025-09-05 07:29:15 -0600

    Add files via upload

---
+++ GestorTareas/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorTareas
+    {
+        public class Tarea
+        {
+            public string Codigo { get; set; }
+            public string Nombre { get; set; }
+            public string Descripcion { get; set; }
+            public DateTime Fecha { get; set; }
+            public string Lugar { get; set; }
+            public string Estado { get; set; }
+        }
+    }
+
+
